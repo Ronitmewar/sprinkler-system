97ba392569f483b19ad1278f0a7d48369d0e2b66
Rohit Mewar <ronit.mewar@gmail.com>
2022-09-03 15:17:04 +0530

    Removed test cases

--- src/test/java/com/sprinklersystem/SprinklerSystemApplicationTests.java
+++ src/test/java/com/sprinklersystem/SprinklerSystemApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sprinklersystem;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SprinklerSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
